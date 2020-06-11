fb9383b66136a0bded3b0a3dd7445b680cb3e0a0
KarthicK43 <63652911+KarthicK43@users.noreply.github.com>
2020-06-11 14:55:35 +0530

    Week 2

--- Day 7/Movie_API/app.js
+++ Day 7/Movie_API/app.js
@@ -23,6 +23,6 @@ app.get("/results",function(req,res){
 	
 })
 
-app.listen(process.env.PORT,process.env.IP,function(){
+app.listen(process.env.PORT||3000,process.env.IP,function(){
 	console.log("Server started");
 })
--- Day 8/YelpCamp_v1/app.js
+++ Day 8/YelpCamp_v1/app.js
@@ -29,6 +29,6 @@ app.post("/actors",function(req,res){
 
 
 
-app.listen(process.env.PORT,process.env.IP,function(){
+app.listen(process.env.PORT||3000,process.env.IP,function(){
 	console.log("Server started");
 })
